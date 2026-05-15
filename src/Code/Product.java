1fdfead6556eed8118749e9f04aeb9c2bbc61e5b
albashzia <ahmedalbashzia@gmail.com>
2026-05-16 03:21:00 +0500

    Defining the default and parameterized constructors of Product class

--- src/Code/Product.java
+++ src/Code/Product.java
@@ -7,4 +7,18 @@ public class Product {
     private double price;
     private int quantity;
 
+    public Product(){
+        this.productID = null;
+        this.name = null;
+        this.price = 0;
+        this.quantity = 0;
+    }
+
+    public Product(String productID, String name, double price, int quantity){
+        this.productID = productID;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
 }
