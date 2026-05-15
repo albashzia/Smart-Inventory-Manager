3e9caea20a6083a1647396d934de930fa9ca9df8
albashzia <ahmedalbashzia@gmail.com>
2026-05-16 03:28:28 +0500

    Defining the default and parameterized constructor of Electronics class

--- src/Code/Electronics.java
+++ src/Code/Electronics.java
@@ -4,5 +4,16 @@ public class Electronics extends Product{
     private double warrantyYears;
     private String brand;
 
+    public Electronics(){
+        super();
+        this.warrantyYears = 0;
+        this.brand = null;
+    }
+
+    public Electronics(String productId, String name, double price, int quantity, double warrantyYears, String brand){
+        super(productId,name,price,quantity);
+        this.warrantyYears = warrantyYears;
+        this.brand = brand;
+    }
 
 }
