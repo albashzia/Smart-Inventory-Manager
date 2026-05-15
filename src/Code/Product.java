07cb854270dffbf686471e7a30d601a13689a8c3
albashzia <ahmedalbashzia@gmail.com>
2026-05-16 03:31:52 +0500

    Creating the setter methods inside Product class

--- src/Code/Product.java
+++ src/Code/Product.java
@@ -21,4 +21,21 @@ public class Product {
         this.quantity = quantity;
     }
 
+
+    public void setProductID(String productID){
+        this.productID = productID;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+    }
+
 }
