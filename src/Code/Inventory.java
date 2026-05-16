662c6619436734f54dc76c6940a4677483245ff4
albashzia <ahmedalbashzia@gmail.com>
2026-05-16 08:58:29 +0500

    Defining Inventory as a Generic class and defining the generic methods to add or delete products

--- src/Code/Inventory.java
+++ src/Code/Inventory.java
@@ -1,4 +1,15 @@
 package Code;
 
-public class Inventory {
+import java.util.ArrayList;
+
+public class Inventory <T extends Product>{
+    ArrayList<T> list = new ArrayList<>();
+
+    public void addProduct(T product){
+        list.add(product);
+    }
+
+    public void deleteProduct(T product){
+        list.remove(product);
+    }
 }
