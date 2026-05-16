ad14d512058e7b24a751c754040bc337979b4028
albashzia <ahmedalbashzia@gmail.com>
2026-05-16 09:19:05 +0500

    Defining a CreateFrame class as a standard class to create frame objects for every window

---
+++ src/GUI/CreateFrame.java
@@ -0,0 +1,12 @@
+package GUI;
+
+import javax.swing.*;
+
+public class CreateFrame extends JFrame {
+    public CreateFrame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(600,400);
+        this.setLocationRelativeTo(null);
+        this.setTitle("Smart Inventory Manager");
+    }
+}
