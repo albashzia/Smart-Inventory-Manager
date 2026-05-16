884f3eb8f9e58908efe3456a2f3f481485797d8b
albashzia <ahmedalbashzia@gmail.com>
2026-05-16 08:47:35 +0500

    Creating the constructors of Furniture class

--- src/Code/Furniture.java
+++ src/Code/Furniture.java
@@ -3,4 +3,16 @@ package Code;
 public class Furniture extends Product{
     private String material;
     private String dimensions;
+
+    public Furniture(){
+        super();
+        this.material = null;
+        this.dimensions = null;
+    }
+
+    public Furniture(String productId, String name, double price, int quantity,String material, String dimensions){
+        super(productId,name,price,quantity);
+        this.material = material;
+        this.dimensions = dimensions;
+    }
 }
