edc16114822166d44f17aa6561a4e2bd3eb188b8
albashzia <ahmedalbashzia@gmail.com>
2026-05-16 09:30:25 +0500

    Creating CreateButton objects inside HomeFrame class and adding them to the frame

--- src/GUI/HomeFrame.java
+++ src/GUI/HomeFrame.java
@@ -2,8 +2,27 @@ package GUI;
 
 public class HomeFrame {
     CreateFrame frame = new CreateFrame();
+    CreateButton addProductFrame;
+    CreateButton removeProductFrame;
+    CreateButton searchProductFrame;
+    CreateButton updateStockFrame;
+    CreateButton exitButton;
 
     public HomeFrame(){
+
+        addProductFrame = new CreateButton("Add Product",100,150);
+        removeProductFrame = new CreateButton("Remove Product",200,150);
+        searchProductFrame = new CreateButton("Search",300,150);
+        updateStockFrame = new CreateButton("Update Stock",400,150);
+        exitButton = new CreateButton("Exit",300,300);
+
+
+
+        frame.add(addProductFrame);
+        frame.add(removeProductFrame);
+        frame.add(searchProductFrame);
+        frame.add(updateStockFrame);
+        frame.add(exitButton);
 
         frame.setVisible(true);
     }
