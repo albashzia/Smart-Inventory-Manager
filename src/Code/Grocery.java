620e36944acbb1d81535d7e7aafef75d0a35c2fc
albashzia <ahmedalbashzia@gmail.com>
2026-05-16 08:43:34 +0500

    Defining the default and parameterized constructor for Grocery class

--- src/Code/Grocery.java
+++ src/Code/Grocery.java
@@ -5,4 +5,16 @@ public class Grocery extends Product{
     private double weight;
 
 
+    public Grocery(){
+        super();
+        this.expiryDate = null;
+        this.weight = 0;
+    }
+
+    public Grocery(String productId, String name, double price, int quantity,String expiryDate, double weight){
+        super(productId,name,price,quantity);
+        this.expiryDate = expiryDate;
+        this.weight = weight;
+    }
+
 }
