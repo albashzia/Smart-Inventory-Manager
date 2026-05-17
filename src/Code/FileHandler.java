a2d588f5666b0d69c67c440c9e42619eebd57426
albashzia <ahmedalbashzia@gmail.com>
2026-05-17 10:43:46 +0500

    Adding a method to write whole Inventory to the file

--- src/Code/FileHandler.java
+++ src/Code/FileHandler.java
@@ -1,4 +1,23 @@
 package Code;
 
+import java.io.*;
+
 public class FileHandler {
+
+    private static final String FILE_NAME = "inventory.dat";
+
+    public static void saveInventory(Inventory<Product> inventory)
+    {
+        try
+        {
+            ObjectOutputStream out =new ObjectOutputStream(new FileOutputStream(FILE_NAME));
+            out.writeObject(inventory.getAllProducts());
+            out.close();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
 }
