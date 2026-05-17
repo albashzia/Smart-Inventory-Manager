634889f479aec4c77a2848ebfc9105886f5ac1dc
albashzia <ahmedalbashzia@gmail.com>
2026-05-17 10:48:32 +0500

    Loading saved inventory when application starts

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import Code.FileHandler;
 import GUI.HomeFrame;
 
 public class Main {
     public static void main(String[] args){
+        FileHandler.loadInventory();
         new HomeFrame();
     }
 }
